8d50a9662e81076e34803085cf9a60c4a018e8ce
wanzi-wanzi <807564472@qq.com>
2022-07-24 14:51:20 +0800

    new vue_test01

---
+++ vue_test01/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>1</h1>
+    <div v-for="(item, index) in my_forms" :key="index">
+       <span v-if="item.name">name:<a id="name">{{ item.name }}</a></span>
+    </div>
+    <div class="v_model">
+      <h1>2</h1>
+      <input v-model="messg">
+      <p>Message is {{messg}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      messg: 'dedededed',
+      my_forms: [{ name: 'wanzi' }, { name: 'shuying' }, { age: 18 }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#name {
+  color: blue;
+}
+.hello {
+background-color: rgb(214, 208, 219);
+border: 1px solid ;
+}
+</style>
